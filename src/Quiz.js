07044aae04575b3a41aa448ea0457c70e764486a
Scott Whigham <scott.whigham@gmail.com>
2020-05-02 10:09:14 -0500

    In part 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,1 +1,17 @@
 import React, { Component } from 'react'
+import { render } from 'enzyme'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {quiz_position: 1};
+    }
+
+    render() {
+        return "<div><div class='QuizQuestion'></div></div>"
+    }
+}
+
+export default Quiz
